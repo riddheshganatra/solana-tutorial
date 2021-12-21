3614b23c2c8ebe1167c7a6f74f9a48d56051b1cf
Riddhesh Ganatra <riddheshganatra@gmail.com>
2021-12-21 15:45:42 +0530

    added buffer conversion part

--- fundedTransaction.js
+++ fundedTransaction.js
@@ -1,83 +1,85 @@
 var web3 = require('@solana/web3.js');
 var splToken = require('@solana/spl-token');
 
-function sleep(time){
-return new Promise((resolve,reject)=>{
-setTimeout(() => {
-  resolve()
-}, time);
-})
+function sleep(time) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve()
+    }, time);
+  })
 }
 
-(async()=>{
-    // connect to solana
-    const connection = new web3.Connection(
-      web3.clusterApiUrl('testnet'),
-      'confirmed',
-      );
+(async () => {
+  // connect to solana
+  const connection = new web3.Connection(
+    web3.clusterApiUrl('testnet'),
+    'confirmed',
+  );
 
-    // generate keypair
-    var fromWallet = web3.Keypair.generate();
-    var toWallet = web3.Keypair.generate();
+  // generate keypair
+  var fromWallet = web3.Keypair.generate();
+  var toWallet = web3.Keypair.generate();
 
-    // get some initial balance. Not possible in production.
-    var fromAirdropSignature = await connection.requestAirdrop(
-        fromWallet.publicKey,
-        10000, // lambports
-      );
-    await connection.confirmTransaction(fromAirdropSignature);
+  // get some initial balance. Not possible in production.
+  var fromAirdropSignature = await connection.requestAirdrop(
+    fromWallet.publicKey,
+    100000, // lambports
+  );
+  await connection.confirmTransaction(fromAirdropSignature);
 
-    console.log(`from wallet airdrop done`);
+  console.log(`from wallet airdrop done`);
 
-    await sleep(5000)
+  await sleep(5000)
 
-    // multi sign transaction, eg: user A wantes to transfer to user B, and user C to fund transaction
-    var feePayerWallet = web3.Keypair.generate();
-    var feePayerAirdropSignature = await connection.requestAirdrop(
-      feePayerWallet.publicKey,
-      1000000000, // lambports
-    );
-    await connection.confirmTransaction(feePayerAirdropSignature);
+  // multi sign transaction, eg: user A wantes to transfer to user B, and user C to fund transaction
+  var feePayerWallet = web3.Keypair.generate();
+  var feePayerAirdropSignature = await connection.requestAirdrop(
+    feePayerWallet.publicKey,
+    10000000, // lambports
+  );
+  await connection.confirmTransaction(feePayerAirdropSignature);
 
-    console.log(`feepayer wallet airdrop done`);
+  console.log(`feepayer wallet airdrop done`);
 
-    await sleep(5000)
+  await sleep(5000)
 
 
 
-    var fundedTransaction = new web3.Transaction({
-      feePayer: feePayerWallet.publicKey
-    }).add(
-      web3.SystemProgram.transfer({
-        fromPubkey: fromWallet.publicKey,
-        toPubkey: toWallet.publicKey,
-        lamports: 10000,
-      }),
-    );
+  var fundedTransaction = new web3.Transaction({
+    feePayer: feePayerWallet.publicKey
+  }).add(
+    web3.SystemProgram.transfer({
+      fromPubkey: fromWallet.publicKey,
+      toPubkey: toWallet.publicKey,
+      lamports: 100,
+    }),
+  );
 
-    // partially sign transaction
-    let blockhashObj = await connection.getRecentBlockhash();
-    fundedTransaction.recentBlockhash = await blockhashObj.blockhash;
+  // partially sign transaction
+  let blockhashObj = await connection.getRecentBlockhash();
+  fundedTransaction.recentBlockhash = await blockhashObj.blockhash;
 
-    fundedTransaction.partialSign(fromWallet);
+  fundedTransaction.partialSign(fromWallet);
 
-    // encoding tranaction so that we can send it to chingari backend
-    let endocdeTransction = fundedTransaction.serialize({
-      requireAllSignatures: false,
-      verifySignatures: false,
-    });
+  // encoding tranaction so that we can send it to chingari backend
+  let endocdeTransction = fundedTransaction.serialize({
+    requireAllSignatures: false,
+    verifySignatures: false,
+  });
 
-    // can be transfered over API if required
-    endocdeTransction = endocdeTransction.toJSON()
+  // can be transfered over API if required
+  endocdeTransction = endocdeTransction.toJSON()
 
-    let transactionFromJson = web3.Transaction.from(endcodedTransction);
+  let transactionFromJson = web3.Transaction.from(Buffer.from(endocdeTransction, 'base64'));
 
-    transactionFromJson.partialSign(feePayerWallet);
+  transactionFromJson.partialSign(feePayerWallet);
 
-    const wireTransaction = transactionFromJson.serialize();
-      const signature = await this.connection.sendRawTransaction(
-        wireTransaction,
-      );
+  const wireTransaction = transactionFromJson.serialize();
+  const signature = await connection.sendRawTransaction(
+    wireTransaction,
+  );
+
+  console.log({ signature });
 
 })()
 
